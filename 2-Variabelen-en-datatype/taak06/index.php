4c0c873315e8bb9bb7d53951f094eaf048c4c94b
Jevancy Bhoelai <jevancybhoelai@gmail.com>
2020-05-27 17:14:39 +0200

    PHP-BASIC-TAAK-06 Datatypes - tonen op het scherm met gettype() AF!!

---
+++ 2-Variabelen-en-datatype/taak06/index.php
@@ -0,0 +1,8 @@
+<?php
+$tekst = 'Hello World!'; // String
+$leeftijd = 20; // Integer
+$prijs = 135.75; // Float
+$check = true; // Boolean
+$var = NULL; //null
+echo gettype($tekst,$leeftijd,$prijs,$check);
+?>

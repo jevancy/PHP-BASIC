660ddd95a69827540bb6f230cd35c1ee3d9b5669
Jevancy Bhoelai <jevancybhoelai@gmail.com>
2020-06-17 18:45:00 +0200

    3-Arrays- PHP-BASIC-TAAK-02 AF!

---
+++ 3-Arrays/taak02/index.php
@@ -0,0 +1,9 @@
+<?php
+$myDogNames = array("Travis Scott", "Roddy Ricch", "Lil Uzi Vert");
+$arrlength = count($myDogNames);
+
+for($x = 0; $x < $arrlength; $x++) {
+  echo $myDogNames[$x];
+  echo "<br>";
+}
+?>

8f7a24af868f6f8c4335016534b9f94a3617ba1f
Jevancy Bhoelai <jevancybhoelai@gmail.com>
2020-06-18 18:30:32 +0200

    4-Arithmatics- PHP-BASIC-TAAK-03 AF!

---
+++ 4-Arithmetics/taak03/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<body>
+<li>
+<?php
+$score = 10;  
+echo ++$score;
+?>  
+
+</body>
+</html>
+<li>
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$score = 10;  
+echo --$score;
+?>  
+
+</body>
+</html>

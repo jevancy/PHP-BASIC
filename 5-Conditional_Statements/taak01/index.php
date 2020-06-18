c3b58c9f2a550f5a07cc473f843fccd765f6e504
Jevancy Bhoelai <jevancybhoelai@gmail.com>
2020-06-18 18:52:22 +0200

    5-Conditional_Statements- PHP-BASIC-TAAK-01 AF!

---
+++ 5-Conditional_Statements/taak01/index.php
@@ -0,0 +1,12 @@
+<?php
+$test1 = 12;
+$test2 = "gewoon tekst";
+
+var_dump($test1 == $test2);
+var_dump($test1 === $test2);
+var_dump($test1 != $test2);
+var_dump($test1 > $test2);
+var_dump($test1 < $test2);
+var_dump($test1 <> $test2);
+var_dump($test1 >= $test2);
+var_dump($test1 <= $test2);

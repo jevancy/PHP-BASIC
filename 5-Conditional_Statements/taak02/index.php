98a7a6b42f104b600598029b43edad5bc5a71289
Jevancy Bhoelai <jevancybhoelai@gmail.com>
2020-06-18 19:34:19 +0200

    5-Conditional_Statements- PHP-BASIC-TAAK-02 AF!

---
+++ 5-Conditional_Statements/taak02/index.php
@@ -0,0 +1,28 @@
+<?php
+$variabele1 = 10;
+$variabele2 = 6;
+
+if($variabele1 != $variabele2) {
+    echo "ongelijk!";
+}
+
+$variabele1 = 10;
+$variabele2 = 10;
+
+if($variabele1 == $variabele2) {
+    echo "identiek!";
+}
+
+$variabele1 = 11;
+$variabele2 = 10;
+
+if($variabele1 > $variabele2) {
+    echo "test1 is groter dan test2!";
+}
+
+$variabele1 = 10;
+$variabele2 = 10;
+
+if($variabele1 >= $variabele2) {
+    echo "test1 is groter of gelijk aan test2!";
+}

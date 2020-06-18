665ea503187e7c65f31971625d64e1fbe1f9db58
Jevancy Bhoelai <jevancybhoelai@gmail.com>
2020-06-18 18:19:47 +0200

    4-Arithmetics- PHP-BASIC-TAAK-02 AF!

---
+++ 4-Arithmetics/taak02/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<body>
+<li>
+<?php
+$x = 20;  
+$x += 100;
+
+echo $x;
+?>  
+
+</body>
+</html>
+<li>
+<?php
+$x = 50;
+$x -= 30;
+
+echo $x;
+?>  
+<li>
+<?php
+$x = 10;  
+$y = 6;
+
+echo $x * $y;
+?>  
+<li>
+<?php
+$x = 10;
+$x /= 5;
+
+echo $x;
+?>  

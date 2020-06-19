c463d11af8aafb62ab1860b531a66afa23f71d22
Jevancy Bhoelai <jevancybhoelai@gmail.com>
2020-06-19 02:05:53 +0200

    6-Functions- PHP-BASIC-TAAK-01 AF!

---
+++ 5-Conditional_Statements/taak03/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$_test1_ = 3;
+$_test2_ = 6;
+
+if($_test1_ == $_test2_) {
+    echo "de vergelijking is waar!";
+}
+else {
+    echo "de vergelijking is niet waar!";
+}
+?>
+ 
+</body>
+</html>

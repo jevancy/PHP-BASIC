73a9180dfb3ad46e9fd6b7c5f427b3c93421d346
Jevancy Bhoelai <jevancybhoelai@gmail.com>
2020-06-22 16:16:17 +0200

    5-Conditional_Statements php-basic taak 06 AF!

--- 5-Conditional_Statements/taak06/index.php
+++ 5-Conditional_Statements/taak06/index.php
@@ -15,3 +15,11 @@ if($var1 < 10 && $var1 > 5) {
 } else {
     echo "het getal is groter dan 10 of kleiner dan 5!";
 }
+
+$var1 = 3;
+
+if($var1 < 10 && $var1 > 5) {
+    echo "het getal zit tussen de 10 en de 5!";
+} else {
+    echo "het getal is groter dan 10 of kleiner dan 5!";
+}
